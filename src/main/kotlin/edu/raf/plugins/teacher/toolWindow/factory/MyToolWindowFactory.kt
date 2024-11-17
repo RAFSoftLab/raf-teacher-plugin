d16243b83257964b93af21d5e79d6dd16a3dac72
Zarko <zarkoned@outloo.com>
2024-11-17 14:03:23 +0100

    separate ui and factory and services

---
+++ src/main/kotlin/edu/raf/plugins/teacher/toolWindow/factory/MyToolWindowFactory.kt
@@ -0,0 +1,15 @@
+package edu.raf.plugins.teacher.toolWindow.factory
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+import edu.raf.plugins.teacher.toolWindow.ui.LoginToolWindow
+
+class MyToolWindowFactory : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val loginToolWindow = LoginToolWindow(project, toolWindow)
+        val content = ContentFactory.getInstance().createContent(loginToolWindow.getContent(), "Login", false)
+        toolWindow.contentManager.addContent(content)
+    }
+}

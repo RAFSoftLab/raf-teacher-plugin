27c6666793cd47e07845c7b496f2e5b30ba60f88
Zarko <zarkoned@outloo.com>
2025-01-20 17:12:25 +0100

    add combobox and history on get student solutions view

--- src/main/kotlin/edu/raf/plugins/teacher/ui/GetStudentSolutionsView.kt
+++ src/main/kotlin/edu/raf/plugins/teacher/ui/GetStudentSolutionsView.kt
@@ -15,6 +15,16 @@ class GetStudentSolutionsView : JPanel() {
         "Korak 4: Izaberite grupu"
     )
 
+    private val options = arrayOf(
+        arrayOf("OOP", "DSW", "TS", "Web Programming"),
+        arrayOf("2022/23", "2023/24", "2024/25"),
+        arrayOf("Prvi test", "Test", "Kolokvijum"),
+        arrayOf("Grupa 1", "Grupa 2", "Grupa 3")
+    )
+
+    private val selectedOptions = mutableMapOf<Int, String>()
+    private val comboBox = JComboBox<String>()
+
     private val subjectsIcon = ImageLoader.loadIcon(ConstantsUtil.SUBJECTS_IMAGE, 30, 30)
     private val calendarIcon = ImageLoader.loadIcon(ConstantsUtil.CALENDAR_IMAGE, 30, 30)
     private val examsIcon = ImageLoader.loadIcon(ConstantsUtil.EXAMS_IMAGE, 30, 30)
@@ -23,70 +33,72 @@ class GetStudentSolutionsView : JPanel() {
     private val prevIcon = ImageIcon(URL(ImageLoader.getImageUrl(ConstantsUtil.PREVIOUS_IMAGE)))
     private val nextIcon = ImageIcon(URL(ImageLoader.getImageUrl(ConstantsUtil.NEXT_IMAGE)))
 
-
-
     private val progressBar = JProgressBar(0, steps.size - 1)
     private val stepLabel = JLabel("", JLabel.LEFT)
     private val stepIconLabel = JLabel()
 
     private val prevButton: JButton = JButton("Prethodni korak").apply {
-        font = font.deriveFont(Font.ITALIC, 12f) // Manji font, stil Italic
-        preferredSize = Dimension(120, 30) // Manje dugme
-        val resizedIcon = ImageIcon(prevIcon.image.getScaledInstance(16, 16, Image.SCALE_SMOOTH)) // Smanjivanje ikone
+        font = font.deriveFont(Font.ITALIC, 12f)
+        preferredSize = Dimension(120, 30)
+        val resizedIcon = ImageIcon(prevIcon.image.getScaledInstance(16, 16, Image.SCALE_SMOOTH))
         icon = resizedIcon
     }
 
     private val nextButton: JButton = JButton("Sledeći korak").apply {
-        font = font.deriveFont(Font.ITALIC, 12f) // Manji font, stil Italic
-        preferredSize = Dimension(120, 30) // Manje dugme
-
-        val resizedIcon = ImageIcon(nextIcon.image.getScaledInstance(16, 16, Image.SCALE_SMOOTH)) // Smanjivanje ikone
+        font = font.deriveFont(Font.ITALIC, 12f)
+        preferredSize = Dimension(120, 30)
+        val resizedIcon = ImageIcon(nextIcon.image.getScaledInstance(16, 16, Image.SCALE_SMOOTH))
         icon = resizedIcon
-
-        horizontalTextPosition = SwingConstants.LEADING // Pozicionira tekst levo
-        verticalTextPosition = SwingConstants.CENTER // Pozicionira tekst vertikalno u centar
+        horizontalTextPosition = SwingConstants.LEADING
+        verticalTextPosition = SwingConstants.CENTER
     }
-
-
 
     init {
         layout = BorderLayout()
         preferredSize = Dimension(400, 300)
 
-        // Panel za ikonu, tekst i progress bar u istom delu
         val topPanel = JPanel()
         topPanel.layout = BoxLayout(topPanel, BoxLayout.Y_AXIS)
         topPanel.alignmentX = Component.LEFT_ALIGNMENT
 
-        val iconTextPanel = JPanel(FlowLayout(FlowLayout.LEFT)) // Slika i tekst u istom redu
+        val iconTextPanel = JPanel(FlowLayout(FlowLayout.LEFT))
         stepIconLabel.preferredSize = Dimension(32, 32)
         stepLabel.font = Font(stepLabel.font.name, Font.PLAIN, 20)
 
         iconTextPanel.add(stepIconLabel)
-        iconTextPanel.add(Box.createHorizontalStrut(10)) // Razmak između slike i teksta
+        iconTextPanel.add(Box.createHorizontalStrut(10))
         iconTextPanel.add(stepLabel)
 
-        // Povećan progress bar odmah ispod teksta
         progressBar.preferredSize = Dimension(350, 15)
         progressBar.foreground = Color(0xDBF7EE)
 
-        // Dodajemo u topPanel
         topPanel.add(iconTextPanel)
-        topPanel.add(Box.createVerticalStrut(5)) // Razmak između teksta i progress bara
+        topPanel.add(Box.createVerticalStrut(5))
         topPanel.add(progressBar)
 
-        // Dugmad za navigaciju
+        val centerPanel = JPanel()
+        centerPanel.layout = GridBagLayout()
+        val gbc = GridBagConstraints()
+        gbc.fill = GridBagConstraints.HORIZONTAL
+        gbc.gridx = 0
+        gbc.gridy = 0
+        gbc.weightx = 0.1  // 60% širine
+        gbc.insets = Insets(10, 0, 10, 0) // Opcioni razmaci
+        centerPanel.add(comboBox, gbc)
+
         val buttonPanel = JPanel(FlowLayout(FlowLayout.CENTER))
-        buttonPanel.border = BorderFactory.createEmptyBorder(5, 0, 0, 0)
+        buttonPanel.border = BorderFactory.createEmptyBorder(25, 0, 0, 0)
         buttonPanel.add(prevButton)
         buttonPanel.add(nextButton)
 
-        add(topPanel, BorderLayout.NORTH) // Pomera sve elemente bliže vrhu
+        add(topPanel, BorderLayout.NORTH)
+        add(centerPanel, BorderLayout.CENTER)
         add(buttonPanel, BorderLayout.SOUTH)
 
         prevButton.isEnabled = false
         prevButton.addActionListener {
             if (currentStep > 0) {
+                selectedOptions[currentStep] = comboBox.selectedItem as String
                 currentStep--
                 updateView()
             }
@@ -94,6 +106,7 @@ class GetStudentSolutionsView : JPanel() {
 
         nextButton.addActionListener {
             if (currentStep < steps.size - 1) {
+                selectedOptions[currentStep] = comboBox.selectedItem as String
                 currentStep++
                 updateView()
             }
@@ -102,14 +115,10 @@ class GetStudentSolutionsView : JPanel() {
         updateView()
     }
 
-
-
-
     private fun updateView() {
         stepLabel.text = steps[currentStep]
         progressBar.value = currentStep
 
-        // Postavljanje odgovarajuće slike za trenutni korak
         stepIconLabel.icon = when (currentStep) {
             0 -> subjectsIcon
             1 -> calendarIcon
@@ -118,9 +127,12 @@ class GetStudentSolutionsView : JPanel() {
             else -> null
         }
 
+        comboBox.removeAllItems()
+        options[currentStep].forEach { comboBox.addItem(it) }
+
+        comboBox.selectedItem = selectedOptions[currentStep] ?: options[currentStep][0]
+
         prevButton.isEnabled = currentStep > 0
         nextButton.isEnabled = currentStep < steps.size - 1
     }
-
-
 }

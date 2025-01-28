fa8718fcdeb7de9bb1ef47e8f3b1eb54ad7c8b18
Zarko <zarkoned@outloo.com>
2025-01-28 16:22:32 +0100

    changed example

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,23 +1,12 @@
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class Config {
-    private static final Properties properties = new Properties();
+    public static final String SERVER_GIT_USERNAME = "raf";
+    public static final String SERVER_USERNAME = "mastersi";
+    public static final String SERVER_PASSWORD = "masterSI2023";
+    public static final String HTTP_REPO_URL = "http://192.168.124.28/";
 
-    static {
-        try {
-            properties.load(new FileInputStream("config.properties"));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load config.properties", e);
-        }
-    }
+    public static final String SERVER_HOST = "192.168.124.28";
 
-    public static final String SERVER_GIT_USERNAME = properties.getProperty("SERVER_GIT_USERNAME");
-    public static final String SERVER_USERNAME = properties.getProperty("SERVER_USERNAME");
-    public static final String SERVER_PASSWORD = properties.getProperty("SERVER_PASSWORD");
-    public static final String HTTP_REPO_URL = properties.getProperty("HTTP_REPO_URL");
-    public static final String SERVER_HOST = properties.getProperty("SERVER_HOST");
-    public static final String REMOTE_SCRIPT_1 = properties.getProperty("REMOTE_SCRIPT_1");
-    public static final int SERVER_SSH_PORT = Integer.parseInt(properties.getProperty("SERVER_SSH_PORT"));
+    public static final String REMOTE_SCRIPT_1 = "sudo /lms-api/setup-git-repo.sh ";
+
+    public static final int SERVER_SSH_PORT = 22;
 }
---
+++ src/main/java/ConfigExample.java
@@ -0,0 +1,12 @@
+public class ConfigExample {
+    public static final String SERVER_GIT_USERNAME = "example_git_username";
+    public static final String SERVER_USERNAME = "example_server_username";
+    public static final String SERVER_PASSWORD = "example_server_password";
+    public static final String HTTP_REPO_URL = "http://examle-server-url/";
+
+    public static final String SERVER_HOST = "example.server.ip";
+
+    public static final String REMOTE_SCRIPT_1 = "sudo /example/path/to/script.sh ";
+
+    public static final int SERVER_SSH_PORT = 22;
+}

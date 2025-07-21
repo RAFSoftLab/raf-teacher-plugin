fa594e2b0110ec11d2104e34f6da150c43b2d0f8
Zarko <zarkoned@outloo.com>
2025-07-21 18:57:10 +0200

    automatski 3

---
+++ src/main/java/Config.java
@@ -0,0 +1,10 @@
+public class Config {
+    public static final String SERVER_GIT_USERNAME = System.getenv("SERVER_GIT_USERNAME");
+    public static final String SERVER_USERNAME = System.getenv("SERVER_USERNAME");
+    public static final String SERVER_PASSWORD = System.getenv("SERVER_PASSWORD");
+    public static final String HTTP_REPO_URL = System.getenv("HTTP_REPO_URL");
+    public static final String SERVER_HOST = System.getenv("SERVER_HOST");
+    public static final String REMOTE_SCRIPT_1 = System.getenv("REMOTE_SCRIPT_1");
+
+    public static final int SERVER_SSH_PORT = Integer.parseInt(System.getenv("SERVER_SSH_PORT"));
+}

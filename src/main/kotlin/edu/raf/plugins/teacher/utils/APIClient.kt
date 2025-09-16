e70457f7751f3e6bc5a8476527e989baf47d0fe8
Zarko <zarkoned@outloo.com>
2025-09-16 14:17:59 +0200

    added config properties as github sercet and sentry to config prop

--- src/main/kotlin/edu/raf/plugins/teacher/utils/APIClient.kt
+++ src/main/kotlin/edu/raf/plugins/teacher/utils/APIClient.kt
@@ -10,28 +10,6 @@ class ApiClient(private val baseUrl: String, private val token: String) {
     private val client = OkHttpClient()
 
     @Throws(IOException::class)
-    fun getTmp(endpoint: String): String {
-        // Kreiramo HTTP klijent
-
-
-        val url = "http://192.168.124.28:8091/api/v1$endpoint"
-        val request = Request.Builder()
-            .url(url)
-            .header("Authorization", "Bearer L2aTA643Z0UJ43bIdBymFExVbpqZg7v5QJafYh6KFRjl04eV6w4TtdppkX41hEwo")
-            .build()
-
-        // Izvršavamo GET zahtev
-        val response: Response = client.newCall(request).execute()
-
-        // Proveravamo da li je odgovor uspešan (status 200)
-        if (!response.isSuccessful) {
-            throw IOException("Nesupesan zahtev: ${response.code}")
-        }
-
-        // Vraćamo telo odgovora kao string
-        return response.body?.string() ?: throw IOException("Prazan odgovor od servera.")
-    }
-
     fun get(endpoint: String): String? {
         val request = Request.Builder()
             .url("$baseUrl$endpoint")
--- src/main/kotlin/edu/raf/plugins/teacher/utils/ConfigLoader.kt
+++ src/main/kotlin/edu/raf/plugins/teacher/utils/ConfigLoader.kt
@@ -1,18 +1,27 @@
 package edu.raf.plugins.teacher.utils
 
+import io.sentry.Sentry
 import java.util.Properties
 
 object ConfigLoader {
     private val properties = Properties()
 
     init {
-        val inputStream = ConfigLoader::class.java.classLoader.getResourceAsStream("config.properties")
-            ?: throw IllegalStateException("Konfiguracioni fajl 'config.properties' nije pronađen!")
-        properties.load(inputStream)
+        runCatching {
+            val inputStream = ConfigLoader::class.java.classLoader.getResourceAsStream("config.properties")
+                ?: throw IllegalStateException("Konfiguracioni fajl 'config.properties' nije pronađen!")
+            properties.load(inputStream)
+        }.onFailure { e ->
+            Sentry.captureException(e)
+        }.getOrThrow()
     }
 
     fun get(key: String): String {
-        return properties.getProperty(key)
-            ?: throw IllegalArgumentException("Konfiguracioni ključ '$key' nije pronađen!")
+        return runCatching {
+            properties.getProperty(key)
+                ?: throw IllegalArgumentException("Konfiguracioni ključ '$key' nije pronađen!")
+        }.onFailure { e ->
+            Sentry.captureException(e)
+        }.getOrThrow()
     }
 }

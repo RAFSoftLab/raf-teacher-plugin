f1f4859080f4c0c4b2b05e32db2db3c28403bbb1
Zarko <zarkoned@outloo.com>
2026-03-21 16:18:22 +0100

    test deploy

--- src/main/kotlin/edu/raf/plugins/teacher/observer/Publisher.kt
+++ src/main/kotlin/edu/raf/plugins/teacher/observer/Publisher.kt
@@ -1,6 +1,7 @@
 package edu.raf.plugins.teacher.observer
 
 interface Publisher {
+    //Test
     fun addSubscriber(subscriber: Subscriber)
     fun removeSubscriber(subscriber: Subscriber)
     fun notifySubscribers(data: Any)
